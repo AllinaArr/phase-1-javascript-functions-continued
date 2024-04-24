c4c3b09efe062ce564b2d7642f809297c14c8294
Alina Rudanets <82982601+AllinaArr@users.noreply.github.com>
2024-04-24 13:16:54 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(defaultValue = "roller-skate") {
+  return "This Saturday, I want to " + defaultValue + "!";
+}
+
+function mondayWork(value = "go to the office") {
+  return `This Monday, I will ${value}.`;
+}
+
+function wrapAdjective(valueDefault = "*") {
+  return function (adjective = "special") {
+    return `You are ${valueDefault}${adjective}${valueDefault}!`;
+  };
+}
